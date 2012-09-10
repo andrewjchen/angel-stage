483bea479bb2f5832ba1bac4be140fa3e23cda60
Kyle Zentner <zentner.kyle@gmail.com>
2012-09-09 22:36:03 -0700

    Fixed test system.

--- src/shared/Position.cxx
+++ src/shared/Position.cxx
@@ -1,12 +1,32 @@
-typedef double coordinate_t;
+#include "Position.hxx"
 
-class Position {
-private:
-  coordinate_t x, y;
+void Position::init(const coordinate_t & x, const coordinate_t & y) {
+  _x = x;
+  _y = y;
+}
 
-public:
-  virtual getX();
-  virtual getY();
-  virtual setX(coordinate_t x);
-  virtual setY(coordinate_t y);
+coordinate_t Position::getX() const {
+  return _x;
+}
+
+coordinate_t Position::getY() const {
+  return _y;
+}
+
+coordinate_t Position::setX(const coordinate_t & x) {
+  _x = x;
+  return x;
+}
+
+coordinate_t Position::setY(const coordinate_t & y) {
+  _y = y;
+  return y;
+}
+
+Position Position::relative(const Position & origin) const {
+  return Position(_x - origin.getX(), _y - origin.getY());
+}
+
+bool Position::within(const coordinate_t & w, const coordinate_t & h) const {
+  return (0 <= _x && _x < w) && (0 <= _y && _y < h);
 }
---
+++ tests/shared/PositionTest.cxx
@@ -0,0 +1,16 @@
+#include "Position.hxx"
+
+int main(int argc, char *argv[]) {
+  Position pos1;
+  pos1.setX(1);
+  pos1.setY(1);
+  Position pos2(2.0, 2.0);
+  Position pos3(-1, -1);
+  if (pos3.getX() != pos1.relative(pos2).getX()) {
+    return 1;
+  }
+  if (pos3.getY() != pos1.relative(pos2).getY()) {
+    return 1;
+  }
+  return 0;
+}
