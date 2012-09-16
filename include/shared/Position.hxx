022e588cfa8a071c13ce37cfea98a82195eefcf5
Kyle Zentner <zentner.kyle@gmail.com>
2012-09-16 15:56:14 -0700

    Added distance method to Position, fixed indents.

--- include/shared/Position.hxx
+++ include/shared/Position.hxx
@@ -5,19 +5,20 @@ typedef double coordinate_t;
 
 class Position {
 private:
-  coordinate_t _x, _y;
+	coordinate_t _x, _y;
 
 public:
-  virtual void init(const coordinate_t & x, const coordinate_t & y);
-  Position(const coordinate_t & x, const coordinate_t & y) {this->init(x, y);}
-  Position() {this->init(0.0, 0.0);}
-  virtual ~Position() {}
-  virtual coordinate_t getX() const;
-  virtual coordinate_t getY() const;
-  virtual coordinate_t setX(const coordinate_t & x);
-  virtual coordinate_t setY(const coordinate_t & y);
-  virtual Position relative(const Position & origin) const;
-  virtual bool within(const coordinate_t & w, const coordinate_t & h) const;
+	virtual void init(const coordinate_t & x, const coordinate_t & y);
+	Position(const coordinate_t & x, const coordinate_t & y) {this->init(x, y);}
+	Position() {this->init(0.0, 0.0);}
+	virtual ~Position() {}
+	virtual coordinate_t getX() const;
+	virtual coordinate_t getY() const;
+	virtual coordinate_t setX(const coordinate_t & x);
+	virtual coordinate_t setY(const coordinate_t & y);
+	virtual Position relative(const Position & origin) const;
+	virtual bool within(const coordinate_t & w, const coordinate_t & h) const;
+	virtual coordinate_t distance(const Position & other) const;
 };
 
 #endif /* _POSITION_H_ */
--- src/shared/Position.cxx
+++ src/shared/Position.cxx
@@ -1,32 +1,39 @@
+#include "math.h"
 #include "Position.hxx"
 
 void Position::init(const coordinate_t & x, const coordinate_t & y) {
-  _x = x;
-  _y = y;
+	_x = x;
+	_y = y;
 }
 
 coordinate_t Position::getX() const {
-  return _x;
+	return _x;
 }
 
 coordinate_t Position::getY() const {
-  return _y;
+	return _y;
 }
 
 coordinate_t Position::setX(const coordinate_t & x) {
-  _x = x;
-  return x;
+	_x = x;
+	return x;
 }
 
 coordinate_t Position::setY(const coordinate_t & y) {
-  _y = y;
-  return y;
+	_y = y;
+	return y;
 }
 
 Position Position::relative(const Position & origin) const {
-  return Position(_x - origin.getX(), _y - origin.getY());
+	return Position(_x - origin.getX(), _y - origin.getY());
 }
 
 bool Position::within(const coordinate_t & w, const coordinate_t & h) const {
-  return (0 <= _x && _x < w) && (0 <= _y && _y < h);
+	return (0 <= _x && _x < w) && (0 <= _y && _y < h);
+}
+
+coordinate_t Position::distance(const Position & other) const {
+	coordinate_t x_diff = abs(other.x - _x);
+	coordinate_t y_diff = abs(other.y - _y);
+	return sqrt(x_diff * x_diff + y_diff * y_diff);
 }
