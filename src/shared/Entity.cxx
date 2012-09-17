be6d227c3b9de310ec3dbbe0e516c5db26fc28d5
Kyle Zentner <zentner.kyle@gmail.com>
2012-09-16 20:48:46 -0700

    Changed components to use static slots.

--- src/shared/Entity.cxx
+++ src/shared/Entity.cxx
@@ -4,11 +4,6 @@ EntityID Entity::get_id() const {
 	return _id;
 }
 
-void Entity::load_component(ComponentType component_type,
-							Component * component) {
-	_components[component_type] = component;
-}
-
 void Entity::react(Event * event) {
 	printf("Entity at %p received event!\n", this);
 	/* TODO: Do things. */
