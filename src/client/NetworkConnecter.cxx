ed5c3adfc27bd01c790fd831c9125ba6b9595ad3
Robert Ou <someh4x0r@gmail.com>
2012-09-23 00:37:42 -0700

    nm-->pt and delete it in NetworkConnector

--- src/client/NetworkConnecter.cxx
+++ src/client/NetworkConnecter.cxx
@@ -21,20 +21,21 @@ NetworkConnecter::~NetworkConnecter(){
 void NetworkConnecter::connect(){
 	if(clientSock == NULL)
 		throw "clientSock is null!";
-	PacketTransporter *nm = new PacketTransporter(clientSock);
+	PacketTransporter *pt = new PacketTransporter(clientSock);
 	IPaddress *ip = SDLNet_TCP_GetPeerAddress(clientSock);
-	nm->peer_ip = ((uint64_t)(ip->host) << 16) | (ip->port);
-	boost::thread *new_peer_thread_read = new boost::thread(boost::bind(PacketTransporter::peer_thread_read, nm));
-	boost::thread *new_peer_thread_write = new boost::thread(boost::bind(PacketTransporter::peer_thread_write, nm));
+	pt->peer_ip = ((uint64_t)(ip->host) << 16) | (ip->port);
+	boost::thread *new_peer_thread_read = new boost::thread(boost::bind(PacketTransporter::peer_thread_read, pt));
+	boost::thread *new_peer_thread_write = new boost::thread(boost::bind(PacketTransporter::peer_thread_write, pt));
 
-	nm->read_thread = new_peer_thread_read;
-	nm->write_thread = new_peer_thread_write;
+	pt->read_thread = new_peer_thread_read;
+	pt->write_thread = new_peer_thread_write;
 
-	packetTransport = nm;
+	packetTransport = pt;
 }
 
 void NetworkConnecter::disconnect(){
 	packetTransport->close();
+	delete packetTransport;
 	/* ClientSock is closed in PacketTransporter::close() */
 	clientSock = NULL;
 	packetTransport = NULL;
